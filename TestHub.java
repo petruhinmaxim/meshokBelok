4ee1099db7e72543748a43afb412e4f7c965337a
petruhinmaxim <petruhinmaxim@gmail.com>
2017-05-13 18:23:10 +0300

    new class

---
+++ TestHub.java
@@ -0,0 +1,12 @@
+/**
+ * Created by petruhinmaxim on 09.05.2017.
+ */
+public class TestHub {
+    public static void main(String[] args) {
+        System.out.println("Hellow Git");
+        Object test1 = new Object();
+        test1.toString()
+    }
+
+
+}
